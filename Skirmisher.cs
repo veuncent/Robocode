4088fd0b7ecf0e9d550d54d61163a0567915aa21
Vincent Meijer <meijer.vincent@gmail.com>
2018-07-27 16:55:14 +0200

    Add initial Skirmisher

---
+++ Skirmisher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robocode;
+
+
+namespace Veuncent
+{
+    public class Skirmisher : Robot
+    {
+        public override void Run()
+        {
+            while (true)
+            {
+                Ahead(20);
+                TurnRight(60);
+            }
+        }
+
+        public override void OnScannedRobot(ScannedRobotEvent e)
+        {
+            Fire(3);
+            TurnLeft(45);
+            Back(80);
+        }
+
+        public override void OnHitWall(HitWallEvent e)
+        {
+            Ahead(120);
+            TurnLeft(100);
+            Ahead(100);
+            TurnLeft(10);
+        }
+    }
+}
